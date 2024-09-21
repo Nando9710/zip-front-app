888b0dfefc521813039eba092752c56b842bbb30
Fernando Carr <fcarr0397@gmail.com>
2024-09-21 19:58:01 -0400

    style: add basic layout to dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,42 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NgOptimizedImage } from '@angular/common';
+import { marker as _t } from '@biesbjerg/ngx-translate-extract-marker';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    NgOptimizedImage
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
-
+  public readonly navList = [
+    {
+      title: _t('Archivos'),
+      icon: 'folder',
+      link: '/dashboard/files'
+    },
+    {
+      title: _t('Usuarios'),
+      icon: 'person',
+      link: '/dashboard/users'
+    }
+  ];
 }
